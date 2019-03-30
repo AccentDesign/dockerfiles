80a42325b62a47821aeee78f76fa363343cf7f76
Stuart George <stuart@accentdesign.co.uk>
2019-03-30 13:00:33 +0000

    drop and recreate db tables for each module

--- starlette/src/tests/conftest.py
+++ starlette/src/tests/conftest.py
@@ -1,43 +1,32 @@
 import pytest
-from starlette.config import environ
-from starlette.testclient import TestClient
 from sqlalchemy import create_engine
-from sqlalchemy_utils import database_exists, create_database, drop_database
+from sqlalchemy_utils import create_database, drop_database
+from starlette.config import environ
 
 environ['TESTING'] = 'TRUE'
 
-from app import settings
-from app import app
-from app.db import Base
+from app import db, settings
 
 
-metadata = Base.metadata
-
-
-@pytest.fixture(autouse=True, scope="session")
-def setup_test_database():
-    """
-    Create a clean test database every time the tests are run.
-    """
+@pytest.fixture(scope="session")
+def engine(request):
     url = str(settings.DATABASE_URL)
+
     engine = create_engine(url)
-    assert not database_exists(url), 'Test database already exists. Aborting tests.'
-    create_database(url)             # Create the test database.
-    metadata.create_all(engine)      # Create the tables.
-    yield                            # Run the tests.
-    drop_database(url)               # Drop the test database.
+    create_database(url)
 
+    def fin():
+        drop_database(url)
 
-@pytest.fixture()
-def client():
-    """
-    Make a 'client' fixture available to test cases.
-    """
-    # Our fixture is created within a context manager. This ensures that
-    # application startup and shutdown run for every test case.
-    #
-    # Because we've configured the DatabaseMiddleware with `rollback_on_shutdown`
-    # we'll get a complete rollback to the initial state after each test case runs.
-    with TestClient(app) as test_client:
-        yield test_client
+    request.addfinalizer(fin)
 
+    return engine
+
+@pytest.fixture(autouse=True, scope="module")
+def create_tables(request, engine):
+    db.Base.metadata.create_all(engine)
+
+    def fin():
+        db.Base.metadata.drop_all(engine)
+
+    request.addfinalizer(fin)
